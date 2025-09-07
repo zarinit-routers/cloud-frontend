5c533ead64372c5448ca6a6dfba683ca12d901a4
Katy248 <petrovanton247@gmail.com>
2025-09-07 20:37:18 +0300

    Added timezone set

--- app/pages/nodes/[id].vue
+++ app/pages/nodes/[id].vue
@@ -4,7 +4,7 @@ import { TOKEN } from "@/consts";
 
 const route = useRoute();
 const id = computed(() => route.params.id);
-const { data, error } = (await useFetch)<{ node: Node }>(() => "/api/clients/" + id.value, {
+const { data, error } = await useFetch<{ node: Node }>(() => "/api/clients/" + id.value, {
     server: false,
     headers: {
         Authorization: TOKEN,
@@ -24,6 +24,24 @@ const { data: commandData } = await useFetch<Response>("/api/cmd/v1/timezone/get
     },
 });
 
+const formTimezone = ref<string>(commandData.value.data.timezone);
+
+const onTimezoneSet = async () => {
+    const { data: commandData } = await useFetch<Response>("/api/cmd/v1/timezone/set", {
+        body: {
+            nodeId: id.value,
+            args: {
+                timezone: formTimezone.value,
+            },
+        },
+        method: "POST",
+        server: false,
+        headers: {
+            Authorization: TOKEN,
+        },
+    });
+};
+
 onMounted(async () => {});
 console.log(commandData.value);
 </script>
@@ -34,10 +52,18 @@ console.log(commandData.value);
 
         <div v-if="commandData">
             <div v-if="commandData.data">
-                Timezone: <span class="font-">{{ commandData.data.timezone }}</span>
+                Timezone: <span>{{ commandData.data.timezone }}</span>
             </div>
             <div v-if="commandData.requestError">{{ commandData.requestError }}</div>
             <div v-if="commandData.commandError">{{ commandData.commandError }}</div>
+            <div v-if="commandData">
+                <form @submit.prevent="onTimezoneSet">
+                    <select v-model="formTimezone">
+                        <option v-for="timezone in Intl.supportedValuesOf('timeZone')" :selected="timezone == commandData.data.timezone" :key="timezone">{{ timezone }}</option>
+                    </select>
+                    <input type="submit" value="Применить" v-show="commandData.data.timezone != formTimezone" />
+                </form>
+            </div>
         </div>
     </div>
 </template>
